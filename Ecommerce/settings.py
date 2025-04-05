8eb9c01ea8af8734508a945229b4c5ce6e92cb23
unknown <mohanraj.ns2020@gmail.com>
2025-04-05 12:14:44 +0530

    Collect static files for production

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -141,8 +141,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),   #for devolopement
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # this is for prod
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
@@ -206,3 +208,6 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+
+
